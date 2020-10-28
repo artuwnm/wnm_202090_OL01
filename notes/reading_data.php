fcc176ee69c9e008e9272f677ebff733f5e2203d
ashleyyeo <70737215+ashleyyeo@users.noreply.github.com>
2020-10-27 23:25:27 -0700

    Add files via upload

--- notes/reading_data.php
+++ notes/reading_data.php
@@ -2,21 +2,8 @@
 
 include "../lib/php/functions.php";
 
-
-//$filename = "notes.json";
-//$file = file_get_contents(filename);
-//$notes_object= json_decode($file);
-
-//$filename = "notes.json";
-//$file = file_get_contents(filename);
-//$notes_object= json_decode($file);
-
-//$notes_object=file_get_json("notes.json");
-//$users_array=file_get_json("../data/users.json");
-
-
-
-//print_p($notes);
+$notes_object = file_get_json("notes.json");
+$users_array = file_get_json("../data/users.json");
 
 
 ?>
@@ -33,25 +20,16 @@ include "../lib/php/functions.php";
 	<?php include "../parts/nav_bar.php"; ?>
 
 	<div class="container">
-		<div class="card soft">
-			<h2>Notes</h2>
-
-			<?php
-
-			for($i=0;$i<count($notes_object->notes);$i++){
-				echo "<li>{$notes_object->notes[$i]}</li>";
-			}
-			?>
-		</div>
+		
 		<div class="card soft">
 			<h2>Users</h2>
 
 			<?php
 
-			for($i=0;$i<count($users_array);$i++){
+			for($i = 0; $i < count($users_array); $i++) {
 				echo "<li>
-					<strong>{$users_array[$i]->name}</strong>
-					<span>{$users_array[$i]->type}</span>
+						<strong>{$users_array[$i]->name}</strong>
+						<span>{$users_array[$i]->type}</span>
 				</li>";
 			}
 			?>
