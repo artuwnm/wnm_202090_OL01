540a1084d4c51afc1c7483c5b108dd7e071d3d23
Michael Garcia <mcgarcia@mcgarcia-mn2.linkedin.biz>
2020-10-30 07:32:52 -0700

    Push admin files to git

--- garcia.michael/README.md
+++ garcia.michael/README.md
@@ -3,3 +3,5 @@
 -http:///michaelgarciaux.com
 -http:///michaelgarciaux.com/aau/wnm608/michaaaeel1
 -http:///michaelgarciaux.com/aau/wnm608/michaaaeel1/styleguide
+-http://michaelgarciaux.com/garcia.michael/notes/reading_data.php
+-http://michaelgarciaux.com/garcia.michael/notes/admin/users.php
--- garcia.michael/parts/meta.php
+++ garcia.michael/parts/meta.php
@@ -1,5 +1,7 @@
 	<meta name="viewport" content="width=device-width">
 
+	<base href="http://michaelgarciaux.com/garcia.michael/">
+
 	<link rel="stylesheet" href="lib/css/styleguide.css">
 	<link rel="stylesheet" href="lib/css/gridsystem.css">
 	<link rel="stylesheet" href="css/storetheme.css">
