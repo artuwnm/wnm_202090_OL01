5f2af6b6c473c76f5657d2012a87969d4402e605
Cinderella-Tian <ningxueyangraduate@gmail.com>
2020-10-31 00:15:57 -0700

    Create meta.php

---
+++ wnm608/Assignment_8.1/parts/meta.php
@@ -0,0 +1,7 @@
+<meta name="viewport" content="width=device-width">
+
+<base href="http://yixiutian.com/wnm608/Assignment_8.1/">
+	
+    <link rel="stylesheet" href="lib/css/gridsystem.css">
+	<link rel="stylesheet" href="lib/css/styleguide.css">
+	<link rel="stylesheet" href="css/storetheme.css">
