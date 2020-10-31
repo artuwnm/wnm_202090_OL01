c52dd8886ad52c379e955e13ff2a2d3afa03e976
Cinderella-Tian <ningxueyangraduate@gmail.com>
2020-10-31 00:15:58 -0700

    Create navbar.php

---
+++ wnm608/Assignment_8.1/parts/navbar.php
@@ -0,0 +1,17 @@
+
+    <header class="navbar">
+    	<div class="container display-flex">
+    		<div class="flex-none">
+    		<h1>Store</h1>
+    	    </div>
+    	<div class="flex-stretch"></div>
+    	<nav class="nav nav-flex flex-none">
+    		<ul>
+\
+    			<li><a href="index.php">Home</a></li>
+    			<li><a href="product_list.php">Product</a></li>
+    			<li><a href="#">About</a></li>
+    		</ul>
+    	</nav>
+        </div>
+    </header>
