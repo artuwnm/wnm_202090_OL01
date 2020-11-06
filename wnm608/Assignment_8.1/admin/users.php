c38bdd354e7296cbc921342cd18e11a816088f58
Cinderella-Tian <ningxueyangraduate@gmail.com>
2020-11-06 15:01:33 -0800

    Update users.php

--- wnm608/Assignment_8.1/admin/users.php
+++ wnm608/Assignment_8.1/admin/users.php
@@ -5,137 +5,106 @@ include "../lib/php/functions.php";
 $users = file_get_json("../data/users.json");
 
 
-
 function showUserPage($user) {
 
 $classes = implode(", ", $user->classes);
 
-    // heredoc
+
 echo <<<HTML
-
 <nav class="nav nav-crumbs">
-   <ul>
-   <li><a href="admin/users.php">Back</a></li>
-   </ul>
-
+	<ul>
+		<li><a href="admin/users.php">Back</a></li>
+	</ul>
 </nav>
-
 <div>
 <form>
-
-<h2>Edit Information</h2>
-
-
-<div class="form-control">
-
-<label class="form-label">Name</label>
-<input type="text" value="$user->name" class="form-input">
-
-
-<label class="form-label">Type</label>
-<input type="text" value="$user->type" class="form-input">
-
-
-<label class="form-label">Email</label>
-<input type="text" value="$user->email" class="form-input">
-
-
-<label class="form-label">Classes</label>
-<input type="text" value="$classes" class="form-input">
-
-
-</div>
-
-
-
-<div class="form-control">
-<a href="admin/users.php" class="form-button">Save</a>
-</div>
-
-
-</form>
-
-</div>
-
-
+	<h2>Edit Information</h2>
+		<div class="form-control">
+			<label class="form-label">Name</label>
+			<input type="text" value="$user->name" class="form-input">
+			<label class="form-label">Type</label>
+			<input type="text" value="$user->type" class="form-input">
+			<label class="form-label">Email</label>
+			<input type="text" value="$user->email" class="form-input">
+			<label class="form-label">Classes</label>
+			<input type="text" value="$classes" class="form-input">
+	</div>
+			<div class="form-control">
+				<a href="admin/users.php" class="form-button">Save</a>
+			</div>
+	</div>
 HTML;
 }
 
 
+
 ?>
 
-
-
-
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-	<meta charset="UTF-8">
-	<title>User Admin Page</title>
-    
-    <?php include "../parts/meta.php"; ?>
-	
+	<title>Users Admin Page </title>
+
+
+	<?php include "../parts/meta.php"; ?>
+
 </head>
+
 <body>
 
-
-
-	
-    <header class="navbar">
-        <div class="container display-flex">
-            <div class="flex-none">
-                <h1>User Admin</h1>
-            </div>
-            <div class="flex-stretch"></div>
-            <nav class="nav nav-flex flex-none">
-                <ul>
-                    <li><a href="admin/user.php">User List</a></li>
-                </ul>
-            </nav>
-        </div>
-    </header>
+	<header class="navbar">
+		<div class="container display-flex">
+			<div class="flex-none">
+				<h1>User Admin</h1>
+			</div>
+			<div class="flex-stretch"></div>
+			<nav class="nav nav-flex flex-none">
+				<ul>
+					<li><a href="admin/users.php">User List</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
 
 
 
-    <div class="container">
-
-
-    	<div class="card soft">
+	<div class="container">
+				<div class="card soft">
 
 
 
-        <?php
+					<?php
 
-        if(isset($_GET['id'])) {
-            showUserPage($users[$_GET['id']]);
+					if(isset($_GET['id'])){
+						showUserPage($users[$_GET['id']]);
 
-        } else {
+					} else {
 
-        ?>
-    		<h2>User List</h2>
-
-            <nav class="nav">
-                <ul>
-
-    		<?php
-
-    		for($i=0;$i<count($users);$i++){
-    			echo "<li>
-    			  <a href='admin/users.php?id=$i'>{$users[$i]->name}</a>
-    			   </li>";
-
-    		}
+					?>
 
 
-    		?>
+					<h2>User List</h2>
 
-        </ul>
-    </nav>
+					<nav class="nav">
+						<ul>
 
 
-    	</div>
-    </div>
+					<?php 
 
+					for($i=0;$i<count($users);$i++){
+						echo "<li>
+							<a href='admin/users.php?id=$i'>{$users[$i]->name}</a>
+							</li>";
+					}
+
+					?>
+
+						</ul>
+					</nav>
+
+						<?php }	?>
+				</div>
+			</div>
 
 
 
