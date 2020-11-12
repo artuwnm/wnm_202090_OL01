ec24d019be96c0db235f85ad95efe98030da129e
szxrdhy33 <514849282@qq.com>
2020-11-12 19:28:19 +0800

    page update

--- sun.cara/product_list.php
+++ sun.cara/product_list.php
@@ -10,91 +10,33 @@
 	<?php ini_set("display_errors","On"); ?>
 
 
-
 	<?php include "parts/navbar.php"; ?>
 
 
 
 	<div class="container">
-		<div class="card soft">
-		<h2>Product List</h2>
-
-	<div class="grid gap">
-		<div class="col-xs-12 col-md-4">
-			<figure class="figure product">
-				<a href="product_item.php?id=1">
-				<img src="https://via.placeholder.com/400x400?text=product" alt="">
-				<figcaption>
-						<div>Product 1</div>
-						<div>$3.99</div>
-				</figcaption>
-				</a>
-			</figure>
-		</div>
-
-		<div  class="col-xs-12 col-md-4">
-			<figure class="figure product">
-				<a href="product_item.php?id=2">
-				<img src="https://via.placeholder.com/400x400?text=product" alt="">
-				<figcaption>
-						<div>Product 2</div>
-						<div>$3.99</div>
-				</figcaption>
-				</a>				
-			</figure>
-		</div>
-
-		<div  class="col-xs-12 col-md-4">
-			<figure class="figure product">
-				<a href="product_item.php?id=3">
-				<img src="https://via.placeholder.com/400x400?text=product" alt="">
-				<figcaption>
-						<div>Product 3</div>
-						<div>$3.99</div>
-				</figcaption>
-				</a>
-			</figure>			
-		</div>
-
-		<div  class="col-xs-12 col-md-4">
-			<figure class="figure product">
-				<a href="product_item.php?id=4">
-				<img src="https://via.placeholder.com/400x400?text=product" alt="">
-				<figcaption>
-						<div>Product 4</div>
-						<div>$3.99</div>
-				</figcaption>
-				</a>
-			</figure>			
-		</div>
-
-		<div  class="col-xs-12 col-md-4">
-			<figure class="figure product">
-				<a href="product_item.php?id=5">
-				<img src="https://via.placeholder.com/400x400?text=product" alt="">
-				<figcaption>
-						<div>Product 5</div>
-						<div>$3.99</div>
-				</figcaption>
-				</a>
-			</figure>			
-		</div>
-
-		<div  class="col-xs-12 col-md-4">
-			<figure class="figure product">
-				<a href="product_item.php?id=6">
-				<img src="https://via.placeholder.com/400x400?text=product" alt="">
-				<figcaption>
-						<div>Product 6</div>
-						<div>$3.99</div>
-				</figcaption>
-				</a>
-			</figure>			
-		</div>
-	</div>
-
+			<h2>Product List</h2>
 	
-	</div>
+
+			<?php
+
+			include_once "lib/php/functions.php";
+			include_once "parts/templates.php";
+
+			$result = makeQuery(
+				makeConn(),
+				"
+				SELECT *
+				FROM `products`
+				ORDER BY `date_create` DESC
+				LIMIT 12
+				"
+			);
+
+			echo "<div class='productlist grid gap'>",array_reduce($result,'productListTemplate'),"</div>";
+
+
+			?>
 	</div>
 
 	<?php include "parts/footer.php"; ?>
