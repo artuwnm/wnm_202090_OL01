b195b1b20de00c8063b84e656d0a1b211733e072
szxrdhy33 <514849282@qq.com>
2020-11-21 03:58:02 +0800

    m11 update

--- sun.cara/parts/navbar.php
+++ sun.cara/parts/navbar.php
@@ -1,4 +1,8 @@
+<?php
 
+include_once "lib/php/functions.php";
+
+?>
 
 <header class="navbar">
 	<div class="container display-flex">
@@ -10,8 +14,11 @@
 			<ul>
 				<li><a href="index.php">Home</a></li>
 				<li><a href="product_list.php">Store</a></li>
-				<li><a href="product_cart.php">Cart</a></li>
 				<li><a href="about.php">About</a></li>
+				<li><a href="product_cart.php">
+				<span>Cart</span>
+				<span class="badge"><?= makeCartBadge(); ?></span>
+			</a></li>
 			</ul>
 		</nav>
 	</div>
