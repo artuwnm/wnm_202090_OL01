ae692827231441a349d11f99b33c500514e18253
Cinderella-Tian <ningxueyangraduate@gmail.com>
2020-11-21 19:47:46 -0800

    Create product_added_to_cart.php

---
+++ wnm608/Assignment_11.1/product_added_to_cart.php
@@ -0,0 +1,57 @@
+<?php 
+
+			include_once "lib/php/functions.php";
+
+			$product = makeQuery(
+		               makeConn(), 
+		               "SELECT * FROM `products` WHERE `id` =".$_GET['id'])[0];
+
+
+			$cart_product = cartItemById($_GET['id']);
+
+	
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>confirmation</title>
+
+
+	<?php include "parts/meta.php"; ?>
+</head>
+
+
+<body>
+
+
+
+<?php include "parts/navbar.php"; ?>
+
+
+
+	<div class="container">
+		<div class="card">
+			<h1 class="home-link"><a href="home.html">V Beauty</a></h1>
+		</div>
+	</div>
+
+
+
+	<div class="container">
+		<div class="card soft">
+			<h2>You added <?= $product->name ?> to your cart.</h2>
+			<p>There are now <?= $cart_product->amount ?> of <?= $product->name ?> in your cart.</p>
+
+			<div class="display-flex">
+				<div class="flex-none"><a href="product_list.php">Continue Shopping</a></div>
+				<div class="flex-stretch"></div>
+				<div class="flex-none"><a href="product_cart.php">Go to Cart</a></div>
+			</div>
+		</div>
+	</div>
+	<?php include "parts/footer.php"; ?>
+</body>
+</html>
