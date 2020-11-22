0fb2733fbdf7db59555447a9b4f70deb40191d91
Cinderella-Tian <ningxueyangraduate@gmail.com>
2020-11-21 19:47:51 -0800

    Create meta.php

---
+++ wnm608/Assignment_11.1/parts/meta.php
@@ -0,0 +1,38 @@
+<meta name="viewport" content="width=device-width">
+	
+    <link rel="stylesheet" href="lib/css/styleguide.css">
+    <link rel="stylesheet" href="lib/css/carousel.css">
+    <link rel="stylesheet" href="css/storetheme.css">
+    <link rel="stylesheet" href="lib/css/gridsystem.css">
+
+
+
+
+
+
+
+
+
+    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
+
+
+
+
+
+
+
+
+
+
+
+    <script src="https://code.jquery.com/jquery-3.5.0.min.js"></script>
+    <script type="text/javascript" src="../lib/js/carousel.js">
+
+    </script>
+        <script type="text/javascript">
+            new Carousel({
+		element: $(".carousel").eq(0),
+		timing: 6000
+	});
+        </script>
