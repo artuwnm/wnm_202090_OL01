f6f0a1b336d3b673a00442f159236ea0d0930444
Cinderella-Tian <ningxueyangraduate@gmail.com>
2020-12-10 15:30:00 -0800

    Create product_cart.php

---
+++ wnm608/Assignment_14.1/product_cart.php
@@ -0,0 +1,46 @@
+<?php
+
+include_once "lib/php/functions.php";
+include_once "parts/templates.php";
+
+//$cart = makeQuery(makeConn(),"SELECT * FROM `products` WHERE `id` IN (4,7,10)");
+
+$cart_items = getCartItems();
+
+
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Cart Page</title>
+
+	<?php include "parts/meta.php"; ?>
+</head>
+<body>
+	<?php ini_set("display_errors","On"); ?>
+
+
+
+	<?php include "parts/navbar.php"; ?>
+
+	<div class="container">
+		<h2>In Your Cart</h2>
+		<div class="grid gap">
+			<div class="col-xs-12 col-md-7">
+				<div class="card soft">
+					<?= array_reduce(getCartItems(), 'cartListTemplate') ?>
+				</div>
+			</div>
+			<div class="col-xs-12 col-md-5">
+				<div class="card soft flat">
+					<?= cartTotals() ?>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	
+<?php include "parts/footer.php"; ?>
+	
+</body>
+</html>
