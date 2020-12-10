1e4b684e55ab88a124237f8d6a80f27cca912c8e
Cinderella-Tian <ningxueyangraduate@gmail.com>
2020-12-10 15:28:09 -0800

    Update templates.php

--- wnm608/Assignment_12.1/parts/templates.php
+++ wnm608/Assignment_12.1/parts/templates.php
@@ -6,7 +6,7 @@ function productListTemplate($r,$o){
 return $r.<<<HTML
 <a class="col-xs-12 col-md-4" href="product_item.php?id=$o->id">
 	<figure class="figure product display-flex flex-column">
-		<div class="flex-stretch image-main">
+		<div class="flex-stretch">
 			<img src="img/$o->thumbnail" alt="">
 		</div>
 		<figcaption class="flex-none">
@@ -85,4 +85,25 @@ return <<<HTML
 	<a href="product_checkout.php" class="form-button">Checkout</a>
 </div>
 HTML;
+}
+
+
+
+
+function recommendedProducts($a) {
+$products = array_reduce($a,'productListTemplate');
+echo <<<HTML
+<div class="grid gap productlist">$products</div>
+HTML;
+}
+
+
+function recommendedCategory($cat,$limit=3) {
+	$result = makeQuery(makeConn(),"SELECT * FROM `products` WHERE `category`='$cat' ORDER BY `date_create` DESC LIMIT $limit");
+	recommendedProducts($result);
+}
+
+function recommendedSimilar($cat,$id=0,$limit=3) {
+	$result = makeQuery(makeConn(),"SELECT * FROM `products` WHERE `category`='$cat' AND `id`<>$id ORDER BY rand() LIMIT $limit");
+	recommendedProducts($result);
 }
